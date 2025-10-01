d41f30fea7d94514c466e08d01a88298b12099ef
Alejrot <alejrot.flaxseed748@passinbox.com>
2025-09-30 22:04:40 -0300

    Contenedores: demo integrador en tres servicios

---
+++ demos/contenedores/integrador/frontend/app/main.py
@@ -0,0 +1,84 @@
+"""main.py - Rutina principal - incluye el maquetado de la pagina web"""
+
+# biblioteca estandar
+from logging import basicConfig
+from logging import info
+from logging import INFO
+
+# paquetes
+import flet as ft
+import requests
+
+# modulos
+from tabla_personas import TablaPersonas
+
+# uso de la consola de logs
+basicConfig(
+    level=INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",  # info incorporada
+)
+
+# URLs al backend
+URL_CREAR_PERSONA = "http://backend_fastapi:8000/nuevo/"
+URL_LEER_TODOS    = "http://backend_fastapi:8000/leer_todos/"
+
+
+# diseño de página
+def main(page: ft.Page):
+    """Esta función define el diseño de la página web."""
+
+    def crear_persona(e):
+        """Este handler ordena crear una nueva persona ficticia."""
+
+        # orden al backend para crear nueva persona
+        requests.post(URL_CREAR_PERSONA, timeout=5)
+        # # relectura de tablas
+        actualizacion_tabla()
+        info("Nueva persona creada")
+
+
+    def actualizacion_tabla():
+        """Esta funcion lee la tabla desde la base de datos y la carga a la página."""
+
+        # lectura de tabla (completa)
+        respuesta = requests.get(URL_LEER_TODOS, timeout=5)
+        personas = respuesta.json()
+
+        # borrado de filas
+        tabla.rows = []
+
+        # # creacion de filas - una a una
+        for persona in personas:
+            info(persona)
+            tabla.nueva_persona(persona)
+
+        # actualizacion grafica
+        page.update()
+
+    # boton flotante
+    page.floating_action_button = ft.FloatingActionButton(
+        icon=ft.Icons.ADD,
+        on_click=crear_persona,
+    )
+
+    # tabla web
+    tabla = TablaPersonas()
+
+    # maquetado y estilos
+    page.add(
+        ft.SafeArea(
+            ft.Container(
+                tabla,
+                alignment=ft.alignment.center,
+                expand=True,
+            )
+        )
+    )
+
+    # actualizacion grafica
+    actualizacion_tabla()
+    page.update()
+
+
+# objeto renderizable por el server Uvicorn
+app = ft.app(main, export_asgi_app=True)
--- demos/contenedores/integrador/frontend/app/tabla_personas.py
+++ demos/contenedores/integrador/frontend/app/tabla_personas.py
@@ -5,7 +5,7 @@ from flet import DataTable, DataCell, DataRow, DataColumn
 from flet import Text, FontWeight
 
 # modulos
-from sql import Persona
+
 
 
 class TablaPersonas(DataTable):
@@ -19,21 +19,24 @@ class TablaPersonas(DataTable):
             heading_row_height=50,
             column_spacing=20,
             columns=[
-                DataColumn(Text("Nombre", weight=FontWeight.BOLD, width=250)),
-                DataColumn(Text("Dirección", weight=FontWeight.BOLD, width=500)),
+                DataColumn(Text("ID", weight=FontWeight.BOLD, width=100)),
+                DataColumn(Text("Nombre", weight=FontWeight.BOLD, width=300)),
+                DataColumn(Text("Dirección", weight=FontWeight.BOLD, width=300)),
                 DataColumn(
                     Text("Edad", weight=FontWeight.BOLD, width=100), numeric=True
                 ),
             ],
         )
 
-    def nueva_persona(self, persona: Persona):
+    # def nueva_persona(self, persona: Persona):
+    def nueva_persona(self, persona: dict):
         """Agrega una fila a la tabla gráfica con los datos de entrada."""
         fila = DataRow(
             cells=[
-                DataCell(Text(persona.nombre, width=250)),
-                DataCell(Text(persona.direccion, width=500)),
-                DataCell(Text(persona.edad, width=100)),
+                DataCell(Text(persona["id"], width=100)),
+                DataCell(Text(persona["nombre"], width=300)),
+                DataCell(Text(persona["direccion"], width=300)),
+                DataCell(Text(persona["edad"], width=100)),
             ],
         )
         self.rows.append(fila)
